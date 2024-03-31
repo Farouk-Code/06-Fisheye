0d3b98a53d9b0ce8e6f1d1b0bb48a6ea6483d43d
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-03-31 17:46:12 +0200

    Fonction creation d'images dans photographer.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  // @ts-ignore
+  modal.style.display = "block";
 }
 
+// @ts-ignore
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  // @ts-ignore
+  modal.style.display = "none";
 }
