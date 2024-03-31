f6076d995890f9d2dd4b0643ca893a39eab6c753
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-03-31 18:49:42 +0200

    Pages d'acceuil finie

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,31 @@
+import { creatPhotographerTemplate } from "../templates/photographer.js";
+
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois récupéré
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+  try {
+    const response = await fetch("../../data/photographers.json");
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
-
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerTemplate(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
+async function displayData(photographers, photographersSection) {
+  for (let photographer of photographers) {
+    const photographerTemplate = creatPhotographerTemplate(photographer);
+    photographersSection.appendChild(photographerTemplate);
+  }
 }
 
 async function init() {
   // Récupère les datas des photographes
   const { photographers } = await getPhotographers();
-  displayData(photographers);
+  console.log(photographers);
+  const photographersSection = document.querySelector(".photographer_section");
+  displayData(photographers, photographersSection);
 }
 
 init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,19 +1,18 @@
-function photographerTemplate(data) {
-  const { name, portrait } = data;
-
+export function creatPhotographerTemplate(data) {
+  const { name, id, city, country, tagline, price, portrait, altname } = data;
   const picture = `assets/photographers/${portrait}`;
 
-  function getUserCardDOM() {
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
-    return article;
-  }
-  return { name, picture, getUserCardDOM };
+  const image = creatImage(picture, altname);
+  const h2 = creatHeading("h2", name);
+  const h3 = creatHeading("h3", `${city}, ${country}`);
+  const tag = creatParagraph(tagline);
+  const rate = creatParagraph(`${price}€/jour`);
+  const imageContainer = creatPhotographeContainer([image]);
+
+  const content = [imageContainer, h2, h3, tag, rate];
+  const article = creatLinkForPhotographe(id, content, name);
+
+  return article;
 }
 
 export function creatImage(src, alt) {
@@ -21,4 +20,44 @@ export function creatImage(src, alt) {
   image.setAttribute("src", src);
   image.setAttribute("alt", alt);
   return image;
+}
+
+export function creatHeading(type, text) {
+  const heading = document.createElement(type);
+  heading.textContent = text;
+  return heading;
+}
+
+export function creatParagraph(text) {
+  const paragpraph = document.createElement("p");
+  paragpraph.textContent = text;
+  return paragpraph;
+}
+
+export function creatPhotographeContainer(children) {
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("photographer_card");
+  imageContainer.setAttribute("tabindex", "0");
+  for (let child of children) {
+    imageContainer.appendChild(child);
+  }
+  return imageContainer;
+}
+
+function creatLinkForPhotographe(id, content, name) {
+  const article = document.createElement("article");
+  const link = creatLink(`photographer.html?id=${id}`);
+  const ariaLabel = `Profil du photographe : ${name}`;
+  link.setAttribute("aria-label", ariaLabel);
+  for (let element of content) {
+    link.appendChild(element);
+  }
+  article.appendChild(link);
+  return article;
+}
+
+function creatLink(href) {
+  const link = document.createElement("a");
+  link.setAttribute("href", href);
+  return link;
 }
